a4759ba98b0625e334206a5842500a34369b0f31
DeniseLuiz <denisesantos.luiz@usp.br>
2021-03-09 20:59:19 -0300

    add variables

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	return 0;
+	
+	// Definindo variáveis:
+	int a;
+	int b;
+	int c;
+	
+	// Solicitando ao usuário:
+	printf("Entre com 3 numeros inteiros diferentes entre si \n");
+	scanf("%i", &a);
+	scanf("%i", &b);
+	scanf("%i", &c);
+	
+	
+}
